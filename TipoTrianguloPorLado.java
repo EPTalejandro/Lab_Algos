8abb7b456b3fc32236f1f0b048bd5a207419d3e4
alejandro <alejandro@localhost.localdomain>
2025-05-23 07:50:40 -0400

    que funcione

--- TipoTrianguloPorLado.java
+++ TipoTrianguloPorLado.java
@@ -1,6 +1,21 @@
 import java.util.Scanner;
 
 public class TipoTrianguloPorLado {
+
+    //@ requires lado1 > 0 && lado2 > 0 && lado3 > 0;
+    //@ ensures \result == 1 <==> (lado1 == lado2 && lado2 == lado3);
+    //@ ensures \result == 2 <==> ((lado1 == lado2 || lado1 == lado3 || lado2 == lado3) && !(lado1 == lado2 && lado2 == lado3));
+    //@ ensures \result == 3 <==> (lado1 != lado2 && lado1 != lado3 && lado2 != lado3);
+
+    public static int tipoTriangulo(int lado1, int lado2, int lado3){
+        if (lado1 == lado2 && lado2 == lado3){
+            return 1;
+        } else if (lado1 == lado2 || lado1==lado3 || lado2==lado3) {
+            return 2;
+        }else {
+            return 3;
+        }
+    }
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int lado1, lado2, lado3;
@@ -12,12 +27,7 @@ public class TipoTrianguloPorLado {
         System.out.print("Ingrese el tercer lado: ");
         lado3 = scanner.nextInt();
 
-        if (lado1 == lado2 && lado2 == lado3) {
-            System.out.println("1");
-        } else if (lado1 == lado2 || lado1 == lado3 || lado2 == lado3) {
-            System.out.println("2");
-        } else {
-            System.out.println("3");
-        }
+        int lado = tipoTriangulo(lado1,lado2,lado3);
+        System.out.println(lado);
     }
 } 
