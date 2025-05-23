74b22140953961ce4d9819579171d60db529c192
sunnyjoke <126696114+sunnyjoke@users.noreply.github.com>
2025-05-23 09:32:31 -0400

    Update EsMatrizDiagonal.java
    Corrección de identaciones y reemplazo de print() por la función return

--- EsMatrizDiagonal.java
+++ EsMatrizDiagonal.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class EsMatrizDiagonal {
 
-    public static void main(String[] args) {
+    public static boolean main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         int[][] matriz;
@@ -11,15 +11,16 @@ public class EsMatrizDiagonal {
         boolean esDiagonal = true;
         
         do {
-        System.out.print("Ingrese el número de filas y columnas (mínimo 2): ");
-        nfilas_columnas = scanner.nextInt();
-        if (nfilas_columnas < 2) {
-            System.out.println("El número debe ser mayor o igual a 2. Intente de nuevo.");
-        }
+            System.out.print("Ingrese el número de filas y columnas (mínimo 2): ");
+            nfilas_columnas = scanner.nextInt();
+            if (nfilas_columnas < 2) {
+                System.out.println("El número debe ser mayor o igual a 2. Intente de nuevo.");
+            }
         } while (nfilas_columnas < 2);
 
         matriz = new int[nfilas_columnas][nfilas_columnas];
         System.out.println("Introduzca los elementos de la matriz:");
+        
         for (int i = 0; i < nfilas_columnas; i++) {
             for (int j = 0; j < nfilas_columnas; j++) {
                 int F = i + 1;
@@ -28,6 +29,7 @@ public class EsMatrizDiagonal {
                 matriz[i][j] = scanner.nextInt();
             }
         }
+        
         for (int i = 0; i < nfilas_columnas; i++) {
             System.out.print("| ");
             for (int j = 0; j < nfilas_columnas; j++) {
@@ -35,14 +37,16 @@ public class EsMatrizDiagonal {
             }
             System.out.println("|");
         }
+        
         for(int i=0;i<nfilas_columnas;i++){
             for(int j=0;j<nfilas_columnas;j++){
                 if(j!=i && matriz[i][j] != 0){
                     esDiagonal = false;
                     break;
-                    }
                 }
             }
-        System.out.println(esDiagonal);
+        }
+        
+        return esDiagonal;
     }
 }
