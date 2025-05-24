209f181eb55272d0236e3d135ca3c72eaa57c398
sunnyjoke <126696114+sunnyjoke@users.noreply.github.com>
2025-05-23 21:59:23 -0400

    Update EsMatrizDiagonal.java
    Corrección y ampliación de JML.

--- EsMatrizDiagonal.java
+++ EsMatrizDiagonal.java
@@ -2,10 +2,9 @@ public class EsMatrizDiagonal {
 
     //@ requires matriz != null;
     //@ requires matriz.length >= 2;
-    //@ requires \forall int i; 0 <= i < matriz.length; matriz[i] != null && matriz[i].length == matriz.length;
+    //@ requires \forall int i; 0 <= i < matriz.length; matriz.length != null;
     //@ ensures \result == (\forall int i, j; 
-    //@                    0 <= i < matriz.length && 0 <= j < matriz[i].length; 
-    //@                    i != j ==> matriz[i][j] == 0);
+    //@                    i != j ==> matrizDiagonal[i][j] == 0);
 
     public static /*@ pure @*/ boolean esMatrizDiagonal(double[] matriz) {
 
@@ -19,14 +18,20 @@ public class EsMatrizDiagonal {
 
         int nfilas_columnas = (int) Math.sqrt(matriz.length);
         matrizDiagonal = new double[nfilas_columnas][nfilas_columnas];
-        
+
+        //@ maintaining 0 <= i < nfilas_columnas
+        //@ maintaining 0 <= j < nfilas_columnas
+        //@ decreasing matriz.length - N
         for (int i = 0; i < nfilas_columnas; i++) {
             for (int j = 0; j < nfilas_columnas; j++) {
                 matrizDiagonal[i][j] = matriz[N];
                 N = N + 1;
             }
         }
-        
+
+        //@ maintaining 0 <= i < nfilas_columnas
+        //@ maintaining 0 <= j < nfilas_columnas
+        //@ decreasing matriz.length - N
         for(int i=0;i<nfilas_columnas;i++){
             for(int j=0;j<nfilas_columnas;j++){
                 if(j!=i && matrizDiagonal[i][j] != 0){
