497e039722758cff282164c406621b764db706d5
alejandro <alejandro@localhost.localdomain>
2025-06-04 15:08:42 -0400

    jml archivo 2

--- Tarea_2/CuentaElementoPosicion.java
+++ Tarea_2/CuentaElementoPosicion.java
@@ -2,6 +2,9 @@ public class CuentaElementoPosicion {
 
     public static int cuentaElementoPosicion(int[] secuencia, int K){
 
+        //@ requires secuencia != null;
+        //@ requieres 0<= k < secuencia.length && secuencia.lenght > 0;
+
         int E = 0;
 
         for(int i = 0; i < secuencia.length; i++){
