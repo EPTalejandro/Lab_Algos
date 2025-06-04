1372b86e7067fcfe2f3c441316af26a63b9528cd
Francisbell Graterol <166448396+Frannieuwu@users.noreply.github.com>
2025-06-04 17:30:55 -0400

    este si es el arrecho

--- Tarea_2/EsPermutacion.java
+++ Tarea_2/EsPermutacion.java
@@ -8,7 +8,7 @@ public class EsPermutacion{
       @ requires S1.length > 0 && S2.length > 0;
       @ requires S1.length == S2.length;
       @ ensures \result == (\forall int z;
-                (\num_of int i; 0 <= i && i < S1.length; S1[i] == z) == (\num_of int i; 0 <= i && i < S2.length; S2[i] == z));
+                (\sum int i; 0 <= i && i < S1.length; S1[i] == z; 1) == (\sum int i; 0 <= i && i < S2.length; S2[i] == z; 1));
       @*/
 
     public static boolean esPermutacion(int[] S1, int[] S2){
@@ -32,14 +32,15 @@ public class EsPermutacion{
 
         /*@ 
           @ requires S != null;
-          @ ensures \result == (\num_of int i; 0 <= i && i < S.length; S[i] == E);
+          @ ensures \result == (\sum int i; 0 <= i && i < S.length; S[i] == E; 1);
+          @ assignable \nothing;
           @*/
     public static int cuentaElementoPosicion(int[] S, int E){
         int r = 0;
         
         /*@
           @ maintaining 0 <= i && i <= S.length;
-          @ maintaining r == (\num_of int j; 0 <= j && j < S.length; S[j] == E);
+          @ maintaining r == (\sum int j; 0 <= j && j < i; S[j] == E; 1);
           @ decreasing S.length - i;
           @*/
         for(int i = 0; i < S.length; i++){
