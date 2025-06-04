168e6631ee25d762fc75a621cef039ec5158d283
Francisbell Graterol <166448396+Frannieuwu@users.noreply.github.com>
2025-06-04 16:19:18 -0400

    Update EsPermutacion.java

--- Tarea_2/EsPermutacion.java
+++ Tarea_2/EsPermutacion.java
@@ -3,11 +3,12 @@ import java.util.ArrayList;
 
 public class EsPermutacion{
 
-    //@requires S1.length > 0 && S2.length > 0;
-    //@requires S1.length == S2.length;
-    //@ensures \result == (\forall int z;
-    //         (\count int i: 0 <= i < S1.length : S1[i] == z) == (\count int i: 0 <= i < S2.length : S2[i] == z));
-
+    /*@ 
+      @ requires S1.length > 0 && S2.length > 0;
+      @ requires S1.length == S2.length;
+      @ ensures \result == (\forall int z;
+                (\num_of int i; 0 <= i && i < S1.length; S1[i] == z) == (\num_of int i; 0 <= i && i < S2.length; S2[i] == z));
+    @*/
 
     public static boolean esPermutacion(int[] S1, int[] S2){
         
@@ -28,15 +29,19 @@ public class EsPermutacion{
         return true;
     }
 
-        //@requires S > 0;
-        //@ensures (\result == (\count int i: 0 <= i < S.length : S[i] == E);
+        /*@ 
+          @ requires S > 0;
+          @ ensures (\result == (\count int i; 0 <= i && i < S.length; S[i] == E);
+        @*/
 
     public static int cuentaElementoPosicion(int[] S, int E){
         int r = 0;
         
-        //@maintaining 0 <= i <= S.length;
-        //@maintaining r == (\count int j: 0 <= j < S.length : S[j] == E);
-        //@decreasing S.length - i;
+        /*@
+          @ maintaining 0 <= i && i <= S.length;
+          @ maintaining r == (\count int j; 0 <= j && j < S.length; S[j] == E);
+          @ decreasing S.length - i;
+        @*/
 
         for(int i = 0; i < S.length; i++){
             if(S[i] == E){
