d56cd8d273910ae54403f6dee347896e21a42779
sunnyjoke <126696114+sunnyjoke@users.noreply.github.com>
2025-06-04 15:36:50 -0400

    sin corrección, solo se arregló la posición del JML

--- Tarea_2/CuentaElementoPosicion.java
+++ Tarea_2/CuentaElementoPosicion.java
@@ -1,10 +1,10 @@
 public class CuentaElementoPosicion {
 
+    //@ requires secuencia != null;
+    //@ requires 0 <= k < secuencia.length && secuencia.lenght > 0;
+    //@ ensures \result == \count int i; 0 <= i < secuencia.length ; secuencia[i] == secuencia[k];
     public static int cuentaElementoPosicion(int[] secuencia, int K){
-
-        //@ requires secuencia != null;
-        //@ requires 0 <= k < secuencia.length && secuencia.lenght > 0;
-        //@ ensures \result == \count int i; 0 <= i < secuencia.length ; secuencia[i] == secuencia[k];
+        
         int E = 0;
 
         //@maintaining 0 <= i < secuencia.lenght;
