d8085ee02d1e29932069a5bcdd012ef7e8039071
sunnyjoke <126696114+sunnyjoke@users.noreply.github.com>
2025-06-04 16:35:48 -0400

    Prueba para suma y pares

--- Tarea_2/PromedioPares.java
+++ Tarea_2/PromedioPares.java
@@ -1,20 +1,23 @@
 public class PromedioPares {
-    
-    //@ requires secuencia != null;
-    //@ requires (\forall int i; 0 <= i < secuencia.length; secuencia[i] >= 0);
-    //@ ensures \result == 0 <==> (\forall int i; 0 <= i < secuencia.length; secuencia[i] % 2 !=0); 
-    //@ ensures \result == (\sum int i; 0 <= i < secuencia.length && secuencia[i] % 2 == 0; secuencia[i]) / (\sum int i; 0 <= i < secuencia.length && secuencia[i] % 2 == 0 ; 1);
 
-    public static /*@ pure @*/ int promedioPares (int[] secuencia){
+    /*@
+        requires secuencia != null;
+        requires (\forall int i; 0 <= i && i < secuencia.length; secuencia[i] >= 0);
+        ensures \result == 0 <==> (\forall int i; 0 <= i && i < secuencia.length; secuencia[i] % 2 !=0); 
+        ensures \result == (\sum int i; 0 <= i < secuencia.length && secuencia[i] % 2 == 0; secuencia[i]) / (\sum int i; 0 <= i < secuencia.length && secuencia[i] % 2 == 0 ; 1);
+    @*/
+    public static int promedioPares (int[] secuencia){
 
         int suma = 0;
         int pares = 0;
         int promedio = 0;
-        
-        //@ maintaining 0 <= i && i < secuencia.length;
-        //@ maintaining suma == (\sum int j; 0 <= j < i && secuencia[j] % 2 == 0; secuencia[j]);
-        //@ maintaining pares == (\sum int j; 0 <= j < i && secuencia[j] % 2 == 0; 1);
-        //@ decreasing secuencia.length - i;
+
+        /*@
+            maintaining 0 <= i && i < secuencia.length;
+            maintaining suma == (\sum int j; 0 <= j < i && secuencia[j] % 2 == 0; secuencia[j]) && suma < Integer.MAX_VALUE;
+            maintaining pares == (\sum int j; 0 <= j < i && secuencia[j] % 2 == 0; 1) && pares < Integer.MAX_VALUE;
+            decreasing secuencia.length - i;
+        @*/
         for (int i = 0; i < secuencia.length; i++){
             if (secuencia[i] % 2 == 0){
                 suma = suma + secuencia[i];
