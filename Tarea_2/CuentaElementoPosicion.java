9c4496e1b39e8faf4c0723f6b12c14d93f7dd485
alejandro <alejandro@localhost.localdomain>
2025-06-04 15:41:53 -0400

    errores ahora si

--- Tarea_2/CuentaElementoPosicion.java
+++ Tarea_2/CuentaElementoPosicion.java
@@ -2,7 +2,8 @@ public class CuentaElementoPosicion {
 
     //@ requires secuencia != null;
     //@ requires 0 <= k < secuencia.length && secuencia.lenght > 0;
-    //@ ensures \result == (\count int i; 0 <= i < secuencia.length ; secuencia[i] == secuencia[k]);
+    //@ ensures \result == \count(int i; 0 <= i < secuencia.length ; secuencia[i] == secuencia[k]);
+
     public static int cuentaElementoPosicion(int[] secuencia, int K){
 
         int E = 0;
