be6cbc049031ea956b83235204aa34999e52bc3a
Francisbell Graterol <166448396+Frannieuwu@users.noreply.github.com>
2025-06-04 16:35:56 -0400

    este si ta yuca espero

--- Tarea_2/EsPermutacion.java
+++ Tarea_2/EsPermutacion.java
@@ -4,11 +4,12 @@ import java.util.ArrayList;
 public class EsPermutacion{
 
     /*@ 
+      @ requires S1 != null && S2 != null;
       @ requires S1.length > 0 && S2.length > 0;
       @ requires S1.length == S2.length;
       @ ensures \result == (\forall int z;
                 (\num_of int i; 0 <= i && i < S1.length; S1[i] == z) == (\num_of int i; 0 <= i && i < S2.length; S2[i] == z));
-    @*/
+      @*/
 
     public static boolean esPermutacion(int[] S1, int[] S2){
         
@@ -30,19 +31,17 @@ public class EsPermutacion{
     }
 
         /*@ 
-          @ requires S > 0;
-          @ ensures (\result == (\count int i; 0 <= i && i < S.length; S[i] == E);
-        @*/
-
+          @ requires S != null;
+          @ ensures \result == (\num_of int i; 0 <= i && i < S.length; S[i] == E);
+          @*/
     public static int cuentaElementoPosicion(int[] S, int E){
         int r = 0;
         
         /*@
           @ maintaining 0 <= i && i <= S.length;
-          @ maintaining r == (\count int j; 0 <= j && j < S.length; S[j] == E);
+          @ maintaining r == \num_of int j; 0 <= j && j < S.length; S[j] == E);
           @ decreasing S.length - i;
-        @*/
-
+          @*/
         for(int i = 0; i < S.length; i++){
             if(S[i] == E){
                 r++;
