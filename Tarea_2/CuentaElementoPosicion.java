3054a608cbe7066cdd5e6799fdefbce39a7b1ec7
sunnyjoke <126696114+sunnyjoke@users.noreply.github.com>
2025-06-06 08:53:28 -0400

    que pereza

--- Tarea_2/CuentaElementoPosicion.java
+++ Tarea_2/CuentaElementoPosicion.java
@@ -11,8 +11,8 @@ public class CuentaElementoPosicion {
 
         /*@
             maintaining 0 <= i && i <= secuencia.length;
-            maintaining E == (\sum int j; 0 <= j && j < i && secuencia[i] == secuencia[K]; 1);
-            maintaining E < Integer.MAX_VALUE;
+            maintaining E == (\sum int j; 0 <= j && j < i && secuencia[j] == secuencia[K]; 1);
+            maintaining 0 <= E && E <= i;
             decreasing secuencia.length - i;
         @*/
         for(int i = 0; i < secuencia.length; i++){
@@ -20,7 +20,6 @@ public class CuentaElementoPosicion {
                 E = E + 1;
             }
         }
-
         return E;
     }
 }
