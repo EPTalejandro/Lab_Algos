fa4c2c0c4286d91ddf75b51f0fb91b2bc6d5c15e
sunnyjoke <126696114+sunnyjoke@users.noreply.github.com>
2025-06-06 07:40:32 -0400

    Arreglados errores con las K, no se por qué falla el .length

--- Tarea_2/CuentaElementoPosicion.java
+++ Tarea_2/CuentaElementoPosicion.java
@@ -1,15 +1,16 @@
 public class CuentaElementoPosicion {
 
-    //@ requires secuencia != null;
-    //@ requires k < secuencia.length && secuencia.lenght > 0;
-    //@ ensures \result == (\sum int i; 0 <= i < secuencia.length && secuencia[i] == secuencia[k]; 1);
-
+    /*@
+        requires secuencia != null && secuencia.length > 0;
+        requires K < secuencia.length;
+        ensures \result == (\sum int i; 0 <= i < secuencia.length && secuencia[i] == secuencia[K]; 1);
+    @*/
     public static int cuentaElementoPosicion(int[] secuencia, int K){
 
         int E = 0;
 
         //@ maintaining 0 <= i < secuencia.lenght;
-        //@ maintaining E == (\sum int i; 0 <= i < secuencia.length && secuencia[i] == secuencia[k]; 1) && E < Integer.MAX_VALUE;
+        //@ maintaining E == (\sum int i; 0 <= i < secuencia.length && secuencia[i] == secuencia[K]; 1) && E < Integer.MAX_VALUE;
         //@ decreasing secuencia.length - i;
         for(int i = 0; i < secuencia.length; i++){
             if(secuencia[i] == secuencia[K]){
