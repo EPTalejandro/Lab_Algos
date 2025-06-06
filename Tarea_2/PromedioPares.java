194173ac37831c31aeb2504c7160b07b1f53d022
sunnyjoke <126696114+sunnyjoke@users.noreply.github.com>
2025-06-06 08:18:19 -0400

    Ya debe funcionar, sólo deberían quedar los warning de las sumas

--- Tarea_2/PromedioPares.java
+++ Tarea_2/PromedioPares.java
@@ -13,9 +13,11 @@ public class PromedioPares {
         int promedio = 0;
 
         /*@
-            maintaining 0 <= i && i < secuencia.length;
-            maintaining suma == (\sum int j; 0 <= j && j < i && secuencia[j] % 2 == 0; secuencia[j]) && suma < Integer.MAX_VALUE;
-            maintaining pares == (\sum int j; 0 <= j && j < i && secuencia[j] % 2 == 0; 1) && pares < Integer.MAX_VALUE;
+            maintaining 0 <= i && i <= secuencia.length;
+            maintaining suma == (\sum int j; 0 <= j && j < i && secuencia[j] % 2 == 0; secuencia[j]);
+            maintaining suma < Integer.MAX_VALUE;
+            maintaining pares == (\sum int j; 0 <= j && j < i && secuencia[j] % 2 == 0; 1);
+            mainttaining pares < Integer.MAX_VALUE;
             decreasing secuencia.length - i;
         @*/
         for (int i = 0; i < secuencia.length; i++){
