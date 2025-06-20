4b800c56346dded82f902e827ea19b342300e21d
EPTalejandro <alejandrodavid15@outlook.com>
2025-06-20 07:34:38 -0400

    retoques inventario

--- Tarea_3/Inventario.java
+++ Tarea_3/Inventario.java
@@ -4,11 +4,14 @@ import java.util.HashSet;
 
 public class Inventario{
     
+    // Crea un nuevo producto 
     public static void añadirProducto(String nombre,int cantidad,double precio){
         new Productos(nombre,cantidad,precio);
     }
 
+    // actualiza la cantidad registrada de dicho producto, el numero actualziacion puede ser negativo o positivo dependiendo si es venta o reposicion
     public static void actulizarCantidad(int actualizacion,Productos producto){
+        //@ensures producto.cantidad + actualizacion >= 0 ;
         if(producto.cantidad + actualizacion >= 0){
             producto.cantidad = producto.cantidad - actualizacion;
         }
@@ -17,25 +20,29 @@ public class Inventario{
         }
     }
 
+    // Lista todos los productos registrados
     public static void listarProductos(){
         HashSet<String> lista = new HashSet<>();
-        for(Productos producto: Productos.productos){
+        for(Productos producto: Productos.listaProductos){
             lista.add(producto.nombre);
         }
         System.out.println("Los productos en inventario son: " + lista);
     }
 
+    // Da la suma de el valor total de el inventario entero
     public static void valorInventario(){
+        //@ ensures valor == (\sum Productos producto;Productos.listaProductos;producto.precio * producto.cantidad)
         double valor = 0;
-        for(Productos producto: Productos.productos){
+        for(Productos producto: Productos.listaProductos){
             valor = valor + (producto.precio * producto.cantidad);
         }
         System.out.println("El valor total de el inventario es: " + valor + "Bolívares");
     }
 
+    // lista todos los productos agotados(aquellos con cantidad = 0)
     public static void productosAgotados(){
         HashSet<String> agotados = new HashSet<>();
-        for(Productos producto: Productos.productos){
+        for(Productos producto: Productos.listaProductos){
             if(producto.cantidad == 0){
                 agotados.add(producto.nombre);
             }
@@ -43,10 +50,11 @@ public class Inventario{
         System.out.println("Los productos agotados son: " + agotados);
     }
 
+    // da el producto con mayor valor actualmente en el inventario tomando en cuenta precio y disponibilidad
     public static void mayorValorTotal(){
         double valor_max = 0;
         String mas_caro = new String();
-        for(Productos producto: Productos.productos){
+        for(Productos producto: Productos.listaProductos){
             if((producto.precio*producto.cantidad) > valor_max){
                 valor_max = producto.precio*producto.cantidad;
                 mas_caro = producto.nombre;
@@ -56,10 +64,11 @@ public class Inventario{
 
     }
 
+    // Da el producto con el valor unitario mas alto 
     public static void productoMasCaro(){
         String mas_caro = new String();
         double valor_max = 0;
-        for(Productos producto: Productos.productos){
+        for(Productos producto: Productos.listaProductos){
             if(producto.precio > valor_max){
                 valor_max = producto.precio;
                 mas_caro = producto.nombre;
--- Tarea_3/Productos.java
+++ Tarea_3/Productos.java
@@ -6,14 +6,16 @@ public class Productos{
     String nombre;
     int cantidad;
     double precio;
-    static HashSet<Productos> productos;
+    static HashSet<Productos> listaProductos = new HashSet<>();
     public Productos(String nombre,int cantidad, double precio){
 
-        //@requires nombre.length > 0 && precio >= 0 && cantidad >= 0;
-
+        //@requires nombre.length > 2 && precio >= 0 && cantidad >= 0;
+        if(nombre.length()<2 || precio < 0 || cantidad < 0){
+            throw new IllegalArgumentException("Los argumentos pasados para registrar un producto son invalidos");
+        }
         this.nombre = nombre;
         this.cantidad = cantidad;
         this.precio = precio;
-        productos.add(this);
+        listaProductos.add(this);
     }
 }
