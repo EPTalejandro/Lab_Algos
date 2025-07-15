cf49a4cacd033b53eb6ec3a756e8af282048d486
EPTalejandro <alejandrodavid15@outlook.com>
2025-07-15 18:10:32 -0400

    punto de prueba

--- Proyecto/Logica.java
+++ Proyecto/Logica.java
@@ -2,8 +2,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Logica{
-
-    static int[][] tablero = new int[9][9];
+    static Graficos.Celd[][] tablero = Graficos.getTablero();
     // el 0 representa cuadros vacios
     // 1 cuadrado verde 2 bola roja 3 bola azul 4 bola amarilla 5 bola blanca 6 bola naranja 7 bola morada
     static int[] elementos = new int[]{1,2,3,4,5,6,7};
@@ -13,13 +12,13 @@ public class Logica{
 
 
     static int puntaje = 0;
-
+    // funcion posiblemente no necesaria 
     public static void iniciarTablero(int[] jugada){
         for(int i=0;i<3;i++){
             int a = (int)(Math.random() * 9);
             int b = (int)(Math.random() * 9);
-            if(tablero[a][b] == 0){
-                tablero[a][b] = jugada[i];
+            if(tablero[a][b].getValor() == 0){
+                tablero[a][b].setValor(jugada[i]);
             }
             else{
                 i = i-1;
@@ -31,13 +30,13 @@ public class Logica{
     // para verificar si la jugada es valida al menos uno de los 8 elementos circundantes que rodean el elemento a mover por ello se revisan los que estan abajo (i=-1) los que estan
     // y todos sus variantes abajo izquierada,derecha y debajo(-1<=j<2) luego verifica los que estan a la misma altura(i=0) se ignora el caso de i y j iguales a 0 ya que es el mismo
     // elemento que estamos verificando finalmente se verifican los de arriba(i=1) y para que no haya errores con la primera y ultima fila y columna siempre se verifica que 
-    // las posiciones a verificar esten dentro de los confines de el tablero
-    public static boolean jugadaEsValida(int f,int c, int[][] posiciones){
+    // las tablero a verificar esten dentro de los confines de el tablero
+    public static boolean jugadaEsValida(int f,int c, Graficos.Celd[][] tablero){
         for(int i=-1;i<2;i++){
             for( int j=-1;j<2;j++){
                 if( i == 0 && j == 0 ){continue;}
                 if(f+i >= 0 && c+j >= 0 && f+i < 9 && c+j < 9){
-                    if(posiciones[f+i][c+j]==0){
+                    if(tablero[f+i][c+j].getValor()==0){
                         return true;
                     }
                 }
@@ -56,15 +55,15 @@ public class Logica{
 
         for(int i = 0; i < 9; i++) {
             for(int j = 0; j < 9; j++) {
-                int a = tablero[i][j];
+                int a = tablero[i][j].getValor();
                 if(a < 2 || a > 7) continue;
 
                 for(int[] d:direcciones) {
                     int ci = i - d[0], cj = j - d[1]; // ci,cj son coordenadas antes de i,j en la misma dirección
-                    if(ci >= 0 && ci < 9 && cj >= 0 && cj < 9 && tablero[ci][cj] == a ) continue;
+                    if(ci >= 0 && ci < 9 && cj >= 0 && cj < 9 && tablero[ci][cj].getValor() == a ) continue;
 
                     int len = 1, ri = i + d[0], rj =  j + d[1]; // son las coordenadas que van después de i,j en la misma dirección
-                    while(ri >= 0 && ri < 9 && rj >= 0 && rj < 9 && tablero[ri][rj] == a ) {
+                    while(ri >= 0 && ri < 9 && rj >= 0 && rj < 9 && tablero[ri][rj].getValor() == a ) {
                         len++; ri += d[0]; rj += d[1];
                     }
                     if(len >= 5) {
@@ -82,12 +81,12 @@ public class Logica{
         for(int size = 9; size >= 2; size--) {  
             for(int i = 0; i <= 9 - size; i++) {
                 for(int j = 0;j <= 9-size; j++) {
-                    if(marcar[i][j] || tablero[i][j] != 1) continue;
+                    if(marcar[i][j] || tablero[i][j].getValor() != 1) continue;
 
                     boolean lleno=true;
                     for(int x = i; x < i + size && lleno; x++)
                         for(int y = j; y < j + size; y++)
-                            if(tablero[x][y] != 1){ lleno = false; break; }
+                            if(tablero[x][y].getValor() != 1){ lleno = false; break; }
 
                     if(lleno) {
                         for(int x = i;x < i + size; x++)
@@ -102,7 +101,7 @@ public class Logica{
         int eliminados = 0;
         for(int i = 0; i < 9; i++)
             for(int j = 0; j < 9; j++)
-                if(marcar[i][j]){ tablero[i][j] = 0; eliminados++; }
+                if(marcar[i][j]){ tablero[i][j].setValor(0); eliminados++; }
 
         if(eliminados>0) {
             puntaje += puntosGanados;
@@ -113,28 +112,43 @@ public class Logica{
         return false;
     }
 
-    public static void siguienteJugada(){
-        int[][] posiciones = Logica.tablero;
+    public static int[] obtenerJugada(){
         Scanner entrada = new Scanner(System.in);
         System.out.println("ingrese la fila del objeto que quiere mover");
         int f = entrada.nextInt() - 1;
         System.out.println("ingrese la columna del objeto que quiere mover");
         int c = entrada.nextInt() - 1;
         // Si quire mover un elemento vacio reinicia la funcion
-        if(posiciones[f][c] == 0){
+        if(tablero[f][c].getValor() == 0){
+            System.out.println("la posicion que desea mover esta vacia");
+            siguienteJugada();
+            return obtenerJugada();
+        }
+        return new int[]{f,c};
+    }
+
+    public static void siguienteJugada(){
+        // int[][] tablero = Graficos.getTablero();
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("ingrese la fila del objeto que quiere mover");
+        int f = entrada.nextInt() - 1;
+        System.out.println("ingrese la columna del objeto que quiere mover");
+        int c = entrada.nextInt() - 1;
+        // Si quire mover un elemento vacio reinicia la funcion
+        if(tablero[f][c].getValor() == 0){
             System.out.println("la posicion que desea mover esta vacia");
             siguienteJugada();
             return;
         }
         //Si el elemento que quiere mover es valido continua con las siguientes comprobaciones
-        if (jugadaEsValida(f, c, posiciones)){
+        if (jugadaEsValida(f, c, tablero)){
             System.out.println("ingrese la fila a donde desea moverlo");
             int fD = entrada.nextInt() - 1;
             System.out.println("ingrese la columna a donde desea moverlo");
             int cD = entrada.nextInt() - 1;
-            if(posiciones[fD][cD] == 0){
-                posiciones[fD][cD] = posiciones[f][c];
-                posiciones[f][c] = 0;
+            if(tablero[fD][cD].getValor() == 0){
+                tablero[fD][cD].setValor(tablero[f][c].getValor());
+                tablero[f][c].setValor(0);
             }
             else{
                 System.out.println("la posicion a donde desea moverlo ya esta ocupado escoja otra");
@@ -149,7 +163,7 @@ public class Logica{
     }
 
     // imprime el estado actual del tablero con una ligera estilizacion grafica
-    public static void imprimirTablero(int[][] posiciones) {
+    public static void imprimirTablero(Graficos.Celd[][] tablero) {
         System.out.print("    1 2 3 4 5 6 7 8 9");
         System.out.println();
         System.out.print("    _ _ _ _ _ _ _ _ _");
@@ -157,7 +171,7 @@ public class Logica{
         for(int i = 0; i < 9; i++) {
             System.out.print((i+1)+" | ");
             for (int j = 0; j < 9; j++) {
-                System.out.print(posiciones[i][j] + " ");
+                System.out.print(tablero[i][j].getValor() + " ");
             }
             System.out.println();
         }
@@ -165,10 +179,9 @@ public class Logica{
 
     // Si hay alguna casilla no vacia significa que el juego no ha terminado 
     public static boolean terminoElJuego(){
-        int[][] posiciones = Logica.tablero;
         for(int i=0;i<9;i++){
             for (int j = 0; j < 9; j++) {
-                if(posiciones[i][j] == 0){
+                if(tablero[i][j].getValor() == 0){
                     return false;
                 }
             }
@@ -188,6 +201,7 @@ public class Logica{
 
     public static void main(String[] args) {
         boolean juego = true;
+        Graficos grafico = new Graficos();
         iniciarTablero(obtenerSiguientesElementos());
         while (juego) { 
             int[] proxima = obtenerSiguientesElementos();
